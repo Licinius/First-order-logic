09e7883b97603d6b836525a07a0c68431d451578
Licinius <florent.dellomo@gmail.com>
2017-03-14 13:56:41 +0100

    Modification
    *Tlp et graph doivent être passé en paramètre car les variables ne sont
    pas global

--- Formule_Logique/ListPredicats.py
+++ Formule_Logique/ListPredicats.py
@@ -6,14 +6,14 @@ class ListPredicats(object):
     
     '''
     dic = {
-     "triangle_1" : Predicat("triangle",arite=1,fun=(lambda args: graph.getIntegerProperty("viewShape")[args[0]] ==tlp.NodeShape.Triangle)),
-     "carre_1" : Predicat("carre",arite=1,fun=(lambda args: graph.getIntegerProperty("viewShape")[args[0]] ==tlp.NodeShape.Square)),
-     "rond_1" : Predicat("rond",arite=1,fun=(lambda args: graph.getIntegerProperty("viewShape")[args[0]] ==tlp.NodeShape.Circle)),
-     "hexagon_1" : Predicat("hexagon",arite=1,fun=(lambda args: graph.getIntegerProperty("viewShape")[args[0]] ==tlp.NodeShape.Hexagon)),
-     "circle_1" : Predicat("circle",arite=1,fun=(lambda args: graph.getIntegerProperty("viewShape")[args[0]] ==tlp.NodeShape.Circle)),
-     "square_1" : Predicat("square",arite=1,fun=(lambda args: graph.getIntegerProperty("viewShape")[args[0]] ==tlp.NodeShape.Square)),
+     "triangle_1" : Predicat("triangle",arite=1,fun=(lambda args: args[1].getIntegerProperty("viewShape")[args[2]] ==args[0].NodeShape.Triangle)),
+     "carre_1" : Predicat("carre",arite=1,fun=(lambda args: args[1].getIntegerProperty("viewShape")[args[2]] ==args[0].NodeShape.Square)),
+     "rond_1" : Predicat("rond",arite=1,fun=(lambda args: args[1].getIntegerProperty("viewShape")[args[2]] ==args[0].NodeShape.Circle)),
+     "hexagon_1" : Predicat("hexagon",arite=1,fun=(lambda args: args[1].getIntegerProperty("viewShape")[args[2]] ==args[0].NodeShape.Hexagon)),
+     "circle_1" : Predicat("circle",arite=1,fun=(lambda args: args[1].getIntegerProperty("viewShape")[args[2]] ==args[0].NodeShape.Circle)),
+     "square_1" : Predicat("square",arite=1,fun=(lambda args: args[1].getIntegerProperty("viewShape")[args[2]] ==args[0].NodeShape.Square)),
      
-     "relier_2" :  Predicat("relier",arite=2,fun=(lambda args : graph.hasEdge(args[0],args[1],False))),
+     "relier_2" :  Predicat("relier",arite=2,fun=(lambda args : args[0].hasEdge(args[1],args[2],False))),
     
      "exemple_1" : Predicat("exemple",arite=1,fun =(lambda args : True))
     }
