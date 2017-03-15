32fe42725ecf8d194005410cae5781c798f50e05
Licinius <florent.dellomo@gmail.com>
2017-03-15 16:46:50 +0100

    Les fonctions c'est bien aussi
    * Simplifier pour enlever les lambdas expressions en fonction

--- Formule_Logique/ListPredicats.py
+++ Formule_Logique/ListPredicats.py
@@ -1,18 +1,36 @@
 from Formule_Logique.Predicat import Predicat
+def triangleFunc(args):
+    if(args[1] is not None):
+        return args[1].getIntegerProperty("viewShape")[args[2]] ==args[0].NodeShape.Triangle
+    else:
+        return args[0].NodeShape.Triangle
+#End func 
+
+def squareFunc(args):
+    if(args[1] is not None):
+        return args[1].getIntegerProperty("viewShape")[args[2]] ==args[0].NodeShape.Square
+    else:
+        return args[0].NodeShape.Square   
+
+def circleFunc(args):
+    if(args[1] is not None):
+        return args[1].getIntegerProperty("viewShape")[args[2]] ==args[0].NodeShape.Circle
+    else:
+        return args[0].NodeShape.Circle  
+    
 class ListPredicats(object): 
     
+ 
     '''
         Ce fichier comprend la liste des predicats que vous voulez ajouter a votre arbre.
     
     '''
     dic = {
-     "triangle_1" : Predicat("triangle",arite=1,fun=(lambda args: (args[1] is not None ) and  (args[1].getIntegerProperty("viewShape")[args[2]] ==args[0].NodeShape.Triangle) or args[0].NodeShape.Triangle)),
-     "carre_1" : Predicat("carre",arite=1,fun=(lambda args: (args[1] is not None ) and  (args[1].getIntegerProperty("viewShape")[args[2]] ==args[0].NodeShape.Square) or args[0].NodeShape.Square)),
-     "rond_1" : Predicat("rond",arite=1,fun=(lambda args: (args[1] is not None ) and  (args[1].getIntegerProperty("viewShape")[args[2]] ==args[0].NodeShape.Square) or args[0].NodeShape.Circle)),
-     "hexagon_1" : Predicat("hexagon",arite=1,fun=(lambda args: (args[1] is not None ) and  (args[1].getIntegerProperty("viewShape")[args[2]] ==args[0].NodeShape.Square) or args[0].NodeShape.Hexagon)),
-     "circle_1" : Predicat("circle",arite=1,fun=(lambda args: (args[1] is not None ) and  (args[1].getIntegerProperty("viewShape")[args[2]] ==args[0].NodeShape.Square) or args[0].NodeShape.Circle)),
-     "square_1" : Predicat("square",arite=1,fun=(lambda args: (args[1] is not None ) and  (args[1].getIntegerProperty("viewShape")[args[2]] ==args[0].NodeShape.Square) or args[0].NodeShape.Square)),
-
+     "triangle_1" : Predicat("triangle",arite=1,fun=triangleFunc),
+     "carre_1" : Predicat("carre",arite=1,fun=squareFunc),
+     "rond_1" : Predicat("rond",arite=1,fun=circleFunc),
+    "circle_1" : Predicat("circle",arite=1,fun=circleFunc),
+     "square_1" : Predicat("square",arite=1,fun=squareFunc),
      "relier_2" :  Predicat("relier",arite=2,fun=(lambda args : args[0].hasEdge(args[1],args[2],False))),
     
      "exemple_1" : Predicat("exemple",arite=1,fun =(lambda args : True))
@@ -28,3 +46,5 @@ class ListPredicats(object):
                 list.append(list[key])     
         return list  
     #end func
+    
+   
